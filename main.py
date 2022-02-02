3c51cb1480ec68c2e6f464cdc26c0f3411eb9489
Bernardo <bernardo@rodartenogueira.com.br>
2022-02-02 10:46:26 -0300

    Versão 1.0

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import pickle
 from time import sleep
 from Pokemon import *
 from Pessoa import *
@@ -31,8 +32,23 @@ def escolherPokemonInicial(player):
         else:
             print("Escolha Inválida.")
 
+def salvarJogo(player):
+    try:
+        with open("database.db", "wb") as arquivo:
+            pickle.dump(player, arquivo)
+            print("Jogo salvo com sucesso!")
+    except Exception as error:
+        print("Erro ao salvar jogo");
+        print(error);
 
-
+def carregarJogo():
+    try:
+        with open("database.db", "rb") as arquivo:
+            player = pickle.load(arquivo);
+            print("Loading feito com sucesso!");
+            return player;
+    except Exception as error:
+        print("Save não encontrado.");
 
 
 if __name__ == "__main__":
@@ -40,46 +56,53 @@ if __name__ == "__main__":
     print(colored('Bem vindo ao mundo dos Pokemons!', 'red'));
     print(colored("=================================", "yellow"))
     
-    sleep(1)
-    nome = input("Qual é o seu nome? ");
-    player = Player(nome);
+    player = carregarJogo();
     
-    print("Olá {}, esse é um mundo habitado por pokemons, a partir de agora sua missão é se tornar um mestre dos pokemons!" .format(nome))
-    sleep(3)
-    print("Capture o máximo de pokemons que conseguir e lute com os seus inimigos.")
-    sleep(1);
-    player.mostrarDinheiro();
+    if not player:
     
-    sleep(2);
-    
-    if player.pokemons:
-        print("Já vi que você já possui pokemons.");
-        sleep(1.5)
-        player.mostrarPokemons();
-    else:
-        print("Parece que você não possui nenhum pokemon, portanto precisa escolher um.");
+        sleep(1)
+        nome = input("Qual é o seu nome? ");
+        player = Player(nome);
+        
+        print("Olá {}, esse é um mundo habitado por pokemons, a partir de agora sua missão é se tornar um mestre dos pokemons!" .format(nome))
+        sleep(3)
+        print("Capture o máximo de pokemons que conseguir e lute com os seus inimigos.")
+        sleep(1);
+        player.mostrarDinheiro();
+        
+        sleep(2);
+        
+        if player.pokemons:
+            print("Já vi que você já possui pokemons.");
+            sleep(1.5)
+            player.mostrarPokemons();
+        else:
+            print("Parece que você não possui nenhum pokemon, portanto precisa escolher um.");
+            sleep(2)
+            escolherPokemonInicial(player);
+        
         sleep(2)
-        escolherPokemonInicial(player);
-    
-    sleep(2)
-    
-    print("Pronto! Agora que você já possui um pokemon, derrote o seu rival Garry.")
-    
-    inimigo = Inimigo("Gary", pokemons=[PokemonAgua("Squirtle", level=1)]);
-    
-    sleep(2)
-    
-    player.batalhar(inimigo);
+        
+        print("Pronto! Agora que você já possui um pokemon, derrote o seu rival Garry.")
+        
+        inimigo = Inimigo("Gary", pokemons=[PokemonAgua("Squirtle", level=1)]);
+        
+        sleep(2)
+        
+        player.batalhar(inimigo);
+        salvarJogo(player);
     
     while True:
         print("----------------------------- ");
         print("1 - Explorar o mundo");
-        print("2 - Lutar com um inimigo.")
-        print("0 - Sair do jogo")
+        print("2 - Lutar com um inimigo.");
+        print("3 - Ver Pokeagenda");
+        print("0 - Sair do jogo");
        
         escolha  = input("O que deseja fazer? ")
         if escolha == "0":
             print("Fechando o jogo...")
+            salvarJogo(player);
             sleep(2);
             break;
         elif escolha == "1":
@@ -87,6 +110,8 @@ if __name__ == "__main__":
         elif escolha == "2":
             inimigoAleatorio = Inimigo();
             player.batalhar(inimigoAleatorio);
+        elif escolha == "3":
+            player.mostrarPokemons();
         else: 
             print("Escolha inválida.");
     
